6df1a98183b7e54e80d8286aa900168b4def03cb
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-03-07 14:57:42 -0800

    login-beta

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-
+      <h1>Friend List</h1>
+      <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+      </ul>
+      <Route path="/login" component={Login} />
       </div>
+      </Router>
     );
   }
 }
