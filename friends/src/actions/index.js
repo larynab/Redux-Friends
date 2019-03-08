a5b2395d15fe4c283cd45fa755089d36b353dbbe
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-03-07 20:42:20 -0800

    I-GETTED-THE-FRIENDS-LIST

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,24 +2,30 @@ import axios from 'axios';
 
 export const LOGIN_START = 'LOGIN_START';
 
+export const ERROR = 'ERROR';
+export const GETTING_FRIENDS = 'GETTING_FRIENDS';
+export const GET_FRIENDS = 'GET_FRIENDS';
+
 export const login = creds => dispatch => {
     dispatch({ type: LOGIN_START });
     return axios.post('http://localhost:5000/api/login', creds)
     .then(res => {localStorage.setItem('token', res.data.payload);
     dispatch({ type: 'LOGIN_SUCCESS', payload: res.data.payload });
-    getFriends();
     })
 }
 
 export const getFriends = () => {
-    axios
-      .get('http://localhost:5000/api/friends', {
-        headers: { Authorization: localStorage.getItem('token') }
-      })
-      .then(res => {
-        console.log(res.data);
+  const friends = axios.get('http://localhost:5000/api/friends', {
+    headers: { Authorization: localStorage.getItem('token') }
+  })
+    return dispatch => {
+      dispatch({ type: GETTING_FRIENDS });
+        friends
+      .then(response => {
+        dispatch({ type: GET_FRIENDS, payload: response.data });
       })
       .catch(err => {
-        console.log(err);
+        dispatch({ type: ERROR, payload: err });
       });
-  };
+    };
+};
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,9 +1,23 @@
+import { ERROR, GETTING_FRIENDS, GET_FRIENDS } from '../actions';
+
 const initialState = {
-    friends: []
+    friends: [],
+    gettingFriends: false,
+    error: null
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case GETTING_FRIENDS:
+            return{ ...state, gettingFriends: true };
+        case GET_FRIENDS:
+            return{ ...state, friends: action.payload, gettingFriends: false };
+        case ERROR:
+            return {
+                ...state,
+                gettingFriends: false,
+                error: action.payload
+            }    
         default: 
             return state;
     }
